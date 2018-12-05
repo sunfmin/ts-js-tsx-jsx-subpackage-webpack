628479a2f098c2a8171c06d55732dc7af8ba63e7
Felix Sun <sunfmin@gmail.com>
2018-12-05 11:39:26 +0800

    use node package in subpackage package.json

--- index.ts
+++ index.ts
@@ -1,20 +1,12 @@
 import hello from './subpackage1/hello';
 import pure from './subpackage1/pure';
 import html from './subpackage1/html';
+import * as React from 'preact';
 
 console.log(html("felix"))
 hello('Felix')
+// hello(1)
 pure()
 
-
-
-// hello(1)
-
-
-// hello("Felix")
-
-
-// import * as React from 'preact';
-
-// React.render(html("Felix"), document.getElementById("root"))
+React.render(html("Felix"), document.getElementById("root"))
 
--- subpackage1/html.tsx
+++ subpackage1/html.tsx
@@ -1,8 +1,10 @@
-import * as React from 'preact'
-import _ from 'lodash/core'
+import * as React from 'preact';
+import * as _ from "lodash";
 
 function article(name: string) : any{
-    return <h1>{name} {_.defaults({ 'a': 1 }, { 'a': 3, 'b': 2 })}</h1>
+    var a1 = _.defaults({ 'a': 1 }, { 'a': 3, 'b': 2 })
+    console.log("a1", a1)
+    return <h1>{name} {a1['b']}</h1>
 }
 
 export default article
